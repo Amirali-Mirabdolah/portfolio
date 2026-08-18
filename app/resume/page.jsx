d5dba773ed419974017d75b976f335920a61524e
amirali mirabdolah <amiralimirabdolah@gmail.com>
2026-08-18 23:37:59 +0330

    feat(UI): init resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
+
+const about = {
+  title: "About Me",
+  description:
+    "I'm a Front-End developer with experience in building web applications using modern technologies. I enjoy solving complex problems and continuously learning new skills to improve my craft.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Seyed Amirali Mirabdolah",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+98 903 78 919 88",
+    },
+    {
+      fieldName: "Gmail",
+      fieldValue: "amiralimirabdolah@gmail.com",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Iranian",
+    },
+  ],
+};
+
+const experience = {
+  icon: "",
+  title: "My Experience",
+  description:
+    "I have experience in building web applications using JS, React, Next.js, and other modern technologies.",
+  items: [
+    {
+      company: "Azhman Madar",
+      position: "Front-End Developer",
+      duration:""
+    },
+  ],
+};
 
 function page() {
-  return (
-    <div>resume page</div>
-  )
+  return <div>resume page</div>;
 }
 
-export default page
+export default page;
