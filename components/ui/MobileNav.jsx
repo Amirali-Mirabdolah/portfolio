50dda49abce57b218dc3c7737032a39b1ce1caf3
amirali mirabdolah <amiralimirabdolah@gmail.com>
2026-09-14 19:58:09 +0330

    feat(MobileNav): remove unused useEffect and streamline menu closing logic

--- components/ui/MobileNav.jsx
+++ components/ui/MobileNav.jsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const links = [
   { name: "home", path: "/" },
@@ -18,9 +18,9 @@ function MobileNav() {
   const pathname = usePathname();
   const [open, setOpen] = useState(false);
 
-  useEffect(() => {
+  const closeMenu = () => {
     setOpen(false);
-  }, [pathname]);
+  };
 
   return (
     <>
@@ -33,7 +33,10 @@ function MobileNav() {
         </SheetTrigger>
         <SheetContent className="flex flex-col">
           <div className="text-white mt-32 mb-40 text-center text-2xl">
-            <Link href="/">
+            <Link
+              href="/"
+              onClick={closeMenu}
+            >
               <h1 className="text-4xl font-semibold">
                 Amirali<span className="text-accent">.</span>
               </h1>
@@ -45,6 +48,7 @@ function MobileNav() {
                 <Link
                   key={index}
                   href={link.path}
+                  onClick={closeMenu}
                   className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
                 >
                   {link.name}
